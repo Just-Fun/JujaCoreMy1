dd738fd1830abe97945446cdea7998a40505ba4f
Just-Fun <Just-Fun@users.noreply.github.com>
2016-01-03 17:59:08 +0200

    sqlcmd Main реализовано select, update, insert, delete,

--- src/main/java/ua/com/juja/sqlcmd/Main.java
+++ src/main/java/ua/com/juja/sqlcmd/Main.java
@@ -1,8 +1,7 @@
 package ua.com.juja.sqlcmd;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Random;
 
 /**
  * Created by serzh on 1/3/16.
@@ -14,7 +13,44 @@ public class Main {
         Class.forName("org.postgresql.Driver");
 
         Connection connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/sqlcmd", "postgres",
-                    "1qwerty");
+                "jdbc:postgresql://localhost:5432/sqlcmd", "postgres",
+                "1qwerty");
+//      insert
+        Statement stmt = connection.createStatement();
+
+        String sql = "INSERT INTO public.user " +
+                "VALUES ('Stiven11', 'Pupkin11')";
+        stmt.executeUpdate(sql);
+
+        stmt.executeUpdate("INSERT INTO public.user (name, password)" +
+                "VALUES ('Stiven12', 'Pupkin12')");
+
+//        select
+        stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM public.user WHERE id > 5");
+        while (rs.next()) {
+            System.out.println("id: " + rs.getString("id"));
+            System.out.println("name: " + rs.getString("name"));
+            System.out.println("password: " + rs.getString("password"));
+            System.out.println("------");
+        }
+        rs.close();
+        stmt.close();
+
+        // delete
+        stmt = connection.createStatement();
+        stmt.executeUpdate("DELETE FROM public.user " +
+                "WHERE id > 10 and id < 100");
+        stmt.close();
+
+        //update
+        PreparedStatement ps = connection.prepareStatement(
+                "UPDATE public.user SET password = ? WHERE id > 3");
+        int pass = new Random().nextInt();
+        ps.setString(1, "password_" + pass);
+        ps.executeUpdate();
+        ps.close();
+
+        connection.close();
     }
 }
