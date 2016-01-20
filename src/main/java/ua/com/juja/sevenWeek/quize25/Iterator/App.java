acf85d9b7a6c01e1551cc4e3f1ee92c33b799b89
Just-Fun <Just-Fun@users.noreply.github.com>
2016-01-20 12:56:19 +0200

    сделал 3-ю колонку в 25 квизе

---
+++ src/main/java/ua/com/juja/sevenWeek/quize25/Iterator/App.java
@@ -0,0 +1,9 @@
+package ua.com.juja.sevenWeek.quize25.Iterator;
+
+/**
+ * Created by serzh on 1/20/16.
+ */
+public class App {
+
+
+}
---
+++ src/main/java/ua/com/juja/sevenWeek/quize25/set/App2.java
@@ -0,0 +1,21 @@
+package ua.com.juja.sevenWeek.quize25.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by serzh on 1/20/16.
+ */
+public class App2 {
+
+    public static void main(String[] args) {
+
+        Set<Boolean> set = new HashSet<>();
+
+        set.remove(set.remove(set.remove(false)));
+
+        System.out.println(set.size());
+
+        System.out.println(set);
+    }
+}
