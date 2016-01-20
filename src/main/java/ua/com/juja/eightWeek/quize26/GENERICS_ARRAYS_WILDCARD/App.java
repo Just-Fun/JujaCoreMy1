dd6a08fa0374165ac7af580434b596ce8868667d
Just-Fun <Just-Fun@users.noreply.github.com>
2016-01-20 19:52:54 +0200

    сделан квиз 26

---
+++ src/main/java/ua/com/juja/eightWeek/quize26/GENERICS_ARRAYS_WILDCARD/App.java
@@ -0,0 +1,18 @@
+package ua.com.juja.eightWeek.quize26.GENERICS_ARRAYS_WILDCARD;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by serzh on 1/20/16.
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<? super Object> list = new ArrayList<Object>();
+
+
+    }
+}
---
+++ src/main/java/ua/com/juja/eightWeek/quize26/GENERICS_WILDCARDS/App.java
@@ -0,0 +1,18 @@
+package ua.com.juja.eightWeek.quize26.GENERICS_WILDCARDS;
+
+import java.util.List;
+
+/**
+ * Created by serzh on 1/20/16.
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+
+        List<?> list0 = null;
+        List<?> list1 = null;
+        list0 = list1;
+
+    }
+}
