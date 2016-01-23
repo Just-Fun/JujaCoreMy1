ce4065db8667dfbf2596a5e5f9e86b12099fea2f
Just-Fun <Just-Fun@users.noreply.github.com>
2016-01-23 19:07:27 +0200

    2. копирую Сашин проект, пока все работает :)

--- src/main/java/ua/com/juja/tryByMyOneSqlCmd/model/JDBCDatabaseManager.java
+++ src/main/java/ua/com/juja/tryByMyOneSqlCmd/model/JDBCDatabaseManager.java
@@ -1,9 +1,41 @@
 package ua.com.juja.tryByMyOneSqlCmd.model;
 
+import java.sql.Connection;
+
 /**
  * Created by serzh on 1/23/16.
  */
-public class JDBCDatabaseManager {
+public class JDBCDatabaseManager implements DatabaseManager{
 
+    private Connection connection;
 
+    @Override
+    public DataSet[] getTableData(String tableName) {
+        return new DataSet[0];
+    }
+
+    @Override
+    public String[] getTableNames() {
+        return new String[0];
+    }
+
+    @Override
+    public void connect(String database, String userName, String password) {
+
+    }
+
+    @Override
+    public void clear(String tableName) {
+
+    }
+
+    @Override
+    public void create(DataSet input) {
+
+    }
+
+    @Override
+    public void update(String tableName, int id, DataSet newValue) {
+
+    }
 }
