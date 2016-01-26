90274b551155cab8285bfe6c7241f2b89495f1f7
Just-Fun <Just-Fun@users.noreply.github.com>
2016-01-26 11:22:22 +0200

    Создал 2 теста в Тесте ListTest

--- src/test/java/ua/com/juja/sqlcmd/controller/command/ListTest.java
+++ src/test/java/ua/com/juja/sqlcmd/controller/command/ListTest.java
@@ -1,9 +1,12 @@
 package ua.com.juja.sqlcmd.controller.command;
 
 import org.junit.Before;
+import org.junit.Test;
 import ua.com.juja.sqlcmd.model.DatabaseManager;
 import ua.com.juja.sqlcmd.view.View;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 /**
@@ -13,10 +16,24 @@ public class ListTest {
 
     private DatabaseManager manager;
     private View view;
+    private Command command;
 
     @Before
     public void setup() {
         manager = mock(DatabaseManager.class);
         view = mock(View.class);
+        command = new List(manager,view);
+    }
+
+    @Test
+    public void canProcessListWithRightParameter() {
+        boolean canProcess = command.canProcess("list");
+        assertTrue(canProcess);
+    }
+
+    @Test
+    public void canProcessListWithWrongParameter() {
+        boolean canProcess = command.canProcess("ghkk");
+        assertFalse(canProcess);
     }
 }
