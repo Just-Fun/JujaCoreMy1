00dc18692cb51ab743ac835b20d6a48c3e2d14c7
Just-Fun <Just-Fun@users.noreply.github.com>
2016-02-04 17:18:35 +0200

    привет мир :)

---
+++ src/main/java/ua/com/juja/patterns/helloWorld/Main.java
@@ -0,0 +1,17 @@
+package ua.com.juja.patterns.helloWorld;
+
+/**
+ * Created by serzh on 2/4/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World!");
+    }
+
+    public void forTest() {
+
+    }
+}
