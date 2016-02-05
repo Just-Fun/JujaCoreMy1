f8fd5fcbec4fa87935d635cddf90ebab50b77298
Just-Fun <Just-Fun@users.noreply.github.com>
2016-02-05 14:03:29 +0200

    сделаны 2 задачки leetcode

--- src/main/java/ua/com/juja/leetcode/A_292_NimGame/Solution.java
+++ src/main/java/ua/com/juja/leetcode/A_292_NimGame/Solution.java
@@ -3,18 +3,31 @@ package ua.com.juja.leetcode.A_292_NimGame;
 /**
  * Created by serzh on 2/4/16.
  */
-/*
-You are playing the following Nim Game with your friend: There is a heap of stones on the table,
-each time one of you take turns to remove 1 to 3 stones. The one who removes the last stone will be the winner.
-You will take the first turn to remove the stones.
-Both of you are very clever and have optimal strategies for the game. Write a function to determine
-whether you can win the game given the number of stones in the heap.
-For example, if there are 4 stones in the heap, then you will never win the game:
-no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your fr
- */
+    // 292
 public class Solution {
-    public boolean canWinNim(int n) {
 
-        return true;
+    public static void main(String[] args) {
+//        int test = 138 / 10;
+//        addDigits(138);
+        System.out.println(addDigits(138));
+    }
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+
+    // 258
+    public static int addDigits(int num) {
+        int temp = 0;
+        while (num > 9) {
+            while (num > 0) {
+                temp += num % 10;
+                num /= 10;
+            }
+            num = temp;
+            temp = 0;
+
+        }
+        return num;
     }
 }
