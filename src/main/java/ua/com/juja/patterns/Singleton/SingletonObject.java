7f4d31d2b918c1b14b13ec9c6d67d1f38ebb3193
Just-Fun <Just-Fun@users.noreply.github.com>
2016-02-05 18:16:37 +0200

    дописал Singleton

--- src/main/java/ua/com/juja/patterns/Singleton/SingletonObject.java
+++ src/main/java/ua/com/juja/patterns/Singleton/SingletonObject.java
@@ -6,13 +6,24 @@ package ua.com.juja.patterns.Singleton;
 public class SingletonObject {
     private static SingletonObject object;
 
+    //    private static String lockObj = "Lock"; // Use for locking
+    static {
+        object = new SingletonObject();
+    }
+
     private SingletonObject() {
     }
 
     public static SingletonObject getInstance() {
-        if (object == null) {
-            object = new SingletonObject();
-        }
+        /*if (object != null) {
+            return object;
+        } else {
+            // Start a synchronized block, only one thread can enter and stay in the block one at a time
+            synchronized (lockObj) {
+                if (object == null) {
+                    object = new SingletonObject();
+                }
+            } // End of synchronized block*/
         return object;
     }
 }
