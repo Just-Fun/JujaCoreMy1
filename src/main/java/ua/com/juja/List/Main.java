ee423d1c82b9f5c149901c3b8e8306f44b285e20
Just-Fun <Just-Fun@users.noreply.github.com>
2016-02-10 18:47:05 +0200

    10.02.2016

--- src/main/java/ua/com/juja/List/Main.java
+++ src/main/java/ua/com/juja/List/Main.java
@@ -1,15 +1,30 @@
 package ua.com.juja.List;
 
+import java.io.*;
+import java.util.Arrays;
+
 /**
  * Created by serzh on 2/8/16.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        MyList list = new LinkedList();
-        list.add("1");
-        list.add("2");
-        System.out.println(list.toString());
-    }
+//       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//       BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+       /* FileReader reader = new FileReader("/home/serzh/Videos/text.txt");
+        char[] buffer = new char[1];
+        StringBuilder builder = new StringBuilder();
+        while (reader.read(buffer) != -1) {
+            builder.append(buffer);
+            System.out.println(builder);
+            System.out.println(Arrays.toString(buffer));
+            System.out.println(new String(buffer));
+            buffer = new char[32];
+        }
+        System.out.println("Whole: " + builder);*/
+
+        InputStream in =
+
 }
