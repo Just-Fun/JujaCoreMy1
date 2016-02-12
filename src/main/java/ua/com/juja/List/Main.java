51e402cc47e7989309ec07c3030118d199e6b06c
serzh <Just-Fun@users.noreply.github.com>
2016-02-12 21:05:20 +0200

    Паттерны головного мозга закончил на 1.11.30

--- src/main/java/ua/com/juja/List/Main.java
+++ src/main/java/ua/com/juja/List/Main.java
@@ -6,10 +6,14 @@ package ua.com.juja.List;
 public class Main {
 
     public static void main(String[] args) {
+        int[][] array = new int[1][3];
 
-        MyList list = new LinkedList();
-        list.add("1");
-        list.add("2");
-        System.out.println(list.toString());
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print("[]");
+            }
+            System.out.println("");
+        }
     }
 }
