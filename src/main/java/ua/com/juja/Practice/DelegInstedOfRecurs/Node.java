d26009f021cc26d0eb08f8095a92fd170ec60779
Just-Fun <Just-Fun@users.noreply.github.com>
2016-02-15 18:13:15 +0200

    15.02.2016

---
+++ src/main/java/ua/com/juja/Practice/DelegInstedOfRecurs/Node.java
@@ -0,0 +1,14 @@
+package ua.com.juja.Practice.DelegInstedOfRecurs;
+
+/**
+ * Created by serzh on 2/15/16.
+ */
+public interface Node {
+
+    int getMaxLeafDepth();
+
+    void addValue(int value);
+
+    String toString();
+
+}
