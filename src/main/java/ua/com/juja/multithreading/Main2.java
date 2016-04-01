aa53164aa8c25eaa0b53bc002a6a91711a6c832d
serzh <Just-Fun@users.noreply.github.com>
2016-04-01 12:42:16 +0300

    quiz 32 2 done

---
+++ src/main/java/ua/com/juja/multithreading/Main2.java
@@ -0,0 +1,11 @@
+package ua.com.juja.multithreading;
+
+/**
+ * Created by serzh on 28.02.16.
+ */
+public class Main2 {
+
+    public static void main(String[] args) {
+        new Thread().run();
+    }
+}
