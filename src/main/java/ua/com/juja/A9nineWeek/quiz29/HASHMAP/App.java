f9ad405d425d10dc0ee8802981dd7dde2627a5a0
Just-Fun <Just-Fun@users.noreply.github.com>
2016-04-27 18:36:44 +0300

    ничего такого

--- src/main/java/ua/com/juja/A9nineWeek/quiz29/HASHMAP/App.java
+++ src/main/java/ua/com/juja/A9nineWeek/quiz29/HASHMAP/App.java
@@ -1,8 +1,6 @@
 package ua.com.juja.A9nineWeek.quiz29.HASHMAP;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by serzh on 1/28/16.
@@ -10,7 +8,7 @@ import java.util.Map;
 public class App {
 
     public static void main(String[] args) {
-        Map<String, String> mapA = new HashMap<>();
+        /*Map<String, String> mapA = new HashMap<>();
         mapA.put("A", "A"); // A=A
         Map<String, String> mapB = new HashMap<>();
         mapB.put("B", "B");// B=B
@@ -19,9 +17,21 @@ public class App {
         mapB.putAll(mapA);// mapB -> C=C,A=A,B=B
         System.out.println(mapA.size() + mapB.size());
 
-        System.out.println(mapA);
+        System.out.println(mapA);*/
 //        System.out.println(map.entrySet().toString());
 
 //        System.out.println(map.size());
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 0);
+        map.put("B", 3);
+        map.put("C", 1);
+        System.out.println(new HashSet<>(map.values()).size());
+        HashSet set = new HashSet<>(map.values());
+        System.out.println(Arrays.toString(set.toArray()));
     }
+
+
+
+
 }
