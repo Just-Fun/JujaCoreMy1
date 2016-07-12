fffdae7eb5c720bdd8d1c456b829a3ec04fcf7d0
Just-Fun <Just-Fun@users.noreply.github.com>
2016-07-12 13:59:48 +0300

    добавил класс Parser для работы с Excel

---
+++ src/main/java/ua/com/juja/FromExcel/Main.java
@@ -0,0 +1,13 @@
+package ua.com.juja.FromExcel;
+
+/**
+ * Created by serzh on 7/12/16.
+ */
+public class Main {
+
+    public static void main(String... args){
+
+        System.out.println(Parser.parse("/home/serzh/Desktop/testfile.xlsx"));
+
+    }
+}
