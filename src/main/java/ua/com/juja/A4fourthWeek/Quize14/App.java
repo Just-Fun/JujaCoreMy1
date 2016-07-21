a62d1ff8ab618b734b8f42b7ccea7014e5d47351
serzh <Just-Fun@users.noreply.github.com>
2016-07-21 08:57:05 +0300

    повторяю поля класса

--- src/main/java/ua/com/juja/A4fourthWeek/Quize14/App.java
+++ src/main/java/ua/com/juja/A4fourthWeek/Quize14/App.java
@@ -4,18 +4,21 @@ package ua.com.juja.A4fourthWeek.Quize14;
  * Created by serzh on 12/15/15.
  */
 public class App {
-    public static int a = 0;
-    public int b = 0;
+    public static int a;
+    public int b;
+    public int c;
 
     public App() {
-        this.b = a++;
-    }
-    public static int get() {
-        return a++ + a++;
+        a++;
+        b++;
+        c = a + b;
     }
     public static void main(String[] args) {
-        System.out.println(get());
-        System.out.println(get());
-        System.out.println(get());
+        App app0 = new App();
+        System.out.println(app0.a);
+        App app1 = new App();
+        System.out.println(app1.b);
+        App app2 = new App();
+        System.out.println(app2.c);
     }
 }
--- src/main/java/ua/com/juja/Practice/Main.java
+++ src/main/java/ua/com/juja/Practice/Main.java
@@ -16,38 +16,45 @@ public class Main {
         cello.getString()[0] = "F";
         System.out.println(cello.getString()[0]);*/
 
-        String s1 = " 6 2 6 ";
-        System.out.println(s1.trim());
+   /*     String s1 = " 6 2 6 ";
+        System.out.println(s1.trim());*/
 
-        String s = reverseByArray("1\2\3^5\4>6<7");
+        String string = "1\2\3^5\4>6<7";
+        System.out.println(string.length());
+        String s = reverseByArray(string);
+        System.out.println(s.length());
+        boolean b = s.contains("\\");
         System.out.println(s);
 
-        String s2 = "RT ";
+        /*String s2 = "RT ";
         System.out.println(reverseByArray(s2.trim()));
 //        String s3 = s2.trim();
         String s3 = reverseByArray(s2.trim());
-        System.out.println(s3);
+        System.out.println(s3);*/
 
-        String a = "123456789";
+        /*String a = "123456789";
         String b = new StringBuffer(a).reverse().toString();
         System.out.println(b);
-
+*/
     }
 
     public static String reverseByArray(String s) {
-        /*StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
         for (int i = s.length() - 1; i >= 0; i--) {
+//            if (s.charAt(i) == '\2') {
+//                return "yes";
+//            }
             builder.append(s.charAt(i));
         }
-        return builder.toString();*/
+        return builder.toString();
 
 
-        char[] a = s.toCharArray();
+        /*char[] a = s.toCharArray();
         char[] b = new char[a.length];
         for (int i = 0; i < a.length; i++) {
             b[(a.length - 1) - i] = a[i];
         }
-        return new String(b);
+        return new String(b);*/
     }
 
     private static int sumLambda(int... num) {
