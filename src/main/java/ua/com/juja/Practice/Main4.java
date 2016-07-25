2e250e30046dc856a5920e5e999b1c909d2bf9b3
Just-Fun <Just-Fun@users.noreply.github.com>
2016-07-25 13:16:34 +0300

    такое...

---
+++ src/main/java/ua/com/juja/Practice/Main4.java
@@ -0,0 +1,14 @@
+package ua.com.juja.Practice;
+
+import java.nio.charset.Charset;
+
+/**
+ * Created by Oleg on 7/25/2016.
+ */
+public class Main4 {
+    public static void main(String[] args) {
+        Charset charset = Charset.defaultCharset();
+
+        System.out.println(charset.displayName());
+    }
+}
