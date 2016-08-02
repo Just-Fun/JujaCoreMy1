82adf43abe1fad10ab288634041136a297cca30e
Just-Fun <Just-Fun@users.noreply.github.com>
2016-08-02 22:37:44 +0300

    filesynch вроде как работает

--- src/main/java/ua/com/juja/InputOutput/FileSynch/Main.java
+++ src/main/java/ua/com/juja/InputOutput/FileSynch/Main.java
@@ -1,77 +1,88 @@
 package ua.com.juja.InputOutput.FileSynch;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.CopyOption;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 
 /**
  * Created by Serzh on 8/2/16.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-//        source = new File("/Users/Serzh/Prog/Temp/Inside1/");
-        File dest = new File("/Users/Serzh/Prog/TempIn/");
-        File test = new File("/Users/Serzh/Prog/Temp1File");
-        File test2 = new File("/Users/Serzh/Prog/TempIn/Inside1");
-        File test3 = new File("/Users/Serzh/Prog/test/Inside1");
+        File temp1File = new File("/Users/Serzh/Prog/Temp1File");
+        File tempDir = new File("/Users/Serzh/Prog/TempDir");
+        File tempFileAndDirs = new File("/Users/Serzh/Prog/TempFileAndDirs");
         File testEmpty = new File("/Users/Serzh/Prog/testEmpty");
-//        copyFilesRecursive(test2, test3);
 
-        File[] list = testEmpty.listFiles();
-        System.out.println(list.length);
-//        checkLength(test, dest);
+        File test = new File("/Users/Serzh/Prog/test");
+
+        checkDirLength(tempFileAndDirs, test);
+
     }
 
-    private static void checkLength(File source, File dest) throws IOException {
+    private static void checkDirLength(File source, File dest) throws IOException {
         if (source.length() != dest.length()) {
-            getFiles(source, dest);
+            fileSynch(source, dest);
         }
     }
 
-    private static void getFiles(File source, File dest) throws IOException {
-        File[] filesFrom = source.listFiles();
-        File[] filesTo = dest.listFiles();
-        // listFiles() возвращает null, если это не директория, а файл
-        if (filesFrom == null & filesTo == null) { // если в обеих по одному файлу
-            replaceIfFilesDifferent(source, dest);
-        } else if (filesFrom == null) {            // если в исходящей только один файл
-            deleteAllExceptSource(source, filesTo);
-            replaceIfFilesDifferent(source, dest);
-        } else if (filesTo == null) {             // если в dest только один файл
-            for (File from : filesFrom) {
+    private static void fileSynch(File src, File dest) throws IOException {
+        File[] srcFiles = src.listFiles();
+        File[] destFiles = dest.listFiles(); // listFiles() возвращает null, если это не директория, а файл
+
+        if (srcFiles == null & destFiles == null) {        // если src, File dest - это файлы, а не папки
+            replaceIfFilesDifferent(src, dest);
+        } else if (srcFiles == null) {                   // если src - это файл, а не папка, а dest - папка или папки
+            File parent = new File(dest.getParent());
+            FileUtils.cleanDirectory(parent);
+            FileUtils.copyFileToDirectory(src, parent);
+        } else if (destFiles == null) {                     // если dest - это файл, а не папка
+            for (File from : srcFiles) {
                 if (from.getName().equals(dest.getName())) {
                     replaceIfFilesDifferent(from, dest);
                 } else {
-//                   copyFilesRecursive();
+                    FileUtils.copyDirectoryToDirectory(from, dest);
                 }
             }
-        } else if (filesFrom.length == 0) { // если source пуста
-            cleanDirectory(dest);
-        } else {
-            deleteIfNotInSource(filesFrom, filesTo);
+        }
+        if (srcFiles.length == 0) {                       // если папка src пуста
+            FileUtils.cleanDirectory(dest);
+        } else {                                             // если в src есть папки(файлы)
+            deleteIfNotInSource(srcFiles, destFiles);
+            findSameCheckCopyAndSoOn(dest, srcFiles, destFiles);
+        }
+    }
 
-            from:
-            for (File from : filesFrom) {
-                to:
-                for (File to : filesTo) {
-                    if (equalsNameAndLength(from, to)) {
-                        continue from;
+    private static void findSameCheckCopyAndSoOn(File dest, File[] srcFiles, File[] destFiles) throws IOException {
+        from:
+        for (File from : srcFiles) {
+            for (File to : destFiles) {
+                if (from.getName().equals(to.getName())) {
+                    if (from.length() == to.length()) {
+                        // do nothing
                     } else {
-                        getFiles(from, to);
+                        fileSynch(from, to);
                     }
+                    continue from;
                 }
-                File innerFolder = getInnerFolder(from, dest);
-                copyFilesRecursive(from, innerFolder);
-                getFiles(from, innerFolder);
             }
+            copyFileOrDirInDir(dest, from);
+        }
+    }
+
+    private static void copyFileOrDirInDir(File dest, File from) throws IOException {
+        if (from.isDirectory()) {
+            FileUtils.copyDirectoryToDirectory(from, dest);
+        } else {
+            FileUtils.copyFileToDirectory(from, dest);
         }
     }
 
     private static void replaceIfFilesDifferent(File source, File dest) throws IOException {
         if (source.length() != dest.length()) {
-            copyFilesRecursive(source, dest);
+            copyDirectory(source, dest);
         }
     }
 
@@ -79,7 +90,7 @@ public class Main {
         return from.getName().equals(to.getName()) && from.length() == to.length();
     }
 
-    private static void deleteIfNotInSource(File[] filesFrom, File[] filesTo) {
+    private static void deleteIfNotInSource(File[] filesFrom, File[] filesTo) throws IOException {
         first:
         for (File dest : filesTo) {
             for (File source : filesFrom) {
@@ -87,23 +98,8 @@ public class Main {
                     continue first;
                 }
             }
-            removeDirectory(dest);
+            FileUtils.deleteQuietly(dest);
         }
-    }
-
-    private static void deleteAllExceptSource(File source, File[] dest) {
-        for (File to : dest) {
-            if (!equalsNameAndLength(source, to)) {
-                removeDirectory(to);
-            }
-        }
-    }
-
-    private static File getInnerFolder(File source, File dest) {
-        String name = source.getName();
-        String absolutePathDest = dest.getAbsolutePath();
-        absolutePathDest += "/" + name;
-        return new File(absolutePathDest);
     }
 
     public static void copyDirectory(File sourceDir, File targetDir) throws IOException {
@@ -125,24 +121,51 @@ public class Main {
         }
     }
 
-    private static void copyFilesRecursive(File source, File dest) throws IOException {
-        CopyOption[] options = new CopyOption[]{
-                StandardCopyOption.REPLACE_EXISTING,
-                StandardCopyOption.COPY_ATTRIBUTES
-        };
 
-        Files.copy(source.toPath(), dest.toPath(), options);
+    // TODO если все ок - ниже все завалить...
+    public static void copyDirectoryUsingApache(String from, String to) throws IOException {
+        File source = new File(from);
+        File target = new File(to);
+        FileUtils.copyDirectory(source, target); //  копирует все, оставляя что было
+    }
 
-        if (source.isDirectory()) {
-            File[] files = source.listFiles();
-            for (File aFile : files) {
-                File temp = getInnerFolder(aFile, dest);
-                copyFilesRecursive(aFile, temp);
+    private static void deleteAllRedundantAndCompareFile(File source, File[] dest) throws IOException {
+        for (File to : dest) {
+            if (!equalsNameAndLength(source, to)) {
+                FileUtils.deleteDirectory(to);
+            } else {
+                replaceIfFilesDifferent(source, to);
             }
         }
     }
 
-    public static void removeDirectory(File dir) {
+    private static File getInnerFolder(File source, File dest) {
+        String name = source.getName();
+        String absolutePathDest = dest.getAbsolutePath();
+        absolutePathDest += "/" + name;
+        return new File(absolutePathDest);
+    }
+
+
+
+    /* private static void copyFilesRecursive(File source, File dest) throws IOException {
+         CopyOption[] options = new CopyOption[]{
+                 StandardCopyOption.REPLACE_EXISTING,
+                 StandardCopyOption.COPY_ATTRIBUTES
+         };
+
+         Files.copy(source.toPath(), dest.toPath(), options);
+
+         if (source.isDirectory()) {
+             File[] files = source.listFiles();
+             for (File aFile : files) {
+                 File temp = getInnerFolder(aFile, dest);
+                 copyFilesRecursive(aFile, temp);
+             }
+         }
+     }
+ */
+   /* public static void removeDirectory(File dir) {
         if (dir.isDirectory()) {
             File[] files = dir.listFiles();
             if (files != null && files.length > 0) {
@@ -154,9 +177,9 @@ public class Main {
         } else {
             dir.delete();
         }
-    }
+    }*/
 
-    public static void cleanDirectory(File dir) {
+    /*public static void cleanDirectory(File dir) {
         if (dir.isDirectory()) {
             File[] files = dir.listFiles();
             if (files != null && files.length > 0) {
@@ -166,5 +189,5 @@ public class Main {
             }
         }
     }
-
+*/
 }
--- src/main/java/ua/com/juja/InputOutput/FileSynch/Main2.java
+++ src/main/java/ua/com/juja/InputOutput/FileSynch/Main2.java
@@ -1,19 +1,25 @@
 package ua.com.juja.InputOutput.FileSynch;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Serzh on 8/2/16.
  */
 public class Main2 {
-    public static void main(String[] args) {
-
-       first: for (int i = 0; i < 3; i++) {
-            second: for (int j = 0; j < 5; j++) {
-                if (j == 3) {
-                    continue first;
-                } else {
-                    System.out.println(i * j);
-                }
-            }
-        }
+    public static void main(String[] args) throws IOException {
+        String from = "/Users/Serzh/Prog/TempIn/Inside1";
+        String to = "/Users/Serzh/Prog/test/Inside1";
+        copyDirectoryUsingApache(from, to);
     }
+
+
+    public static void copyDirectoryUsingApache(String from, String to) throws IOException {
+        File source = new File(from);
+        File target = new File(to);
+        FileUtils.copyDirectory(source, target);
+    }
+
 }
