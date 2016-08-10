30add64cb214127203f17720a83a3e6c078794ac
Just-Fun <Just-Fun@users.noreply.github.com>
2016-08-10 22:23:49 +0300

    по мелочам

---
+++ src/main/java/ua/com/juja/Collections/jenerics/Person.java
@@ -0,0 +1,12 @@
+package ua.com.juja.Collections.jenerics;
+
+/**
+ * Created by Serzh on 8/10/16.
+ */
+public class Person implements Comparable<Person>{
+
+    @Override
+    public int compareTo(Person o) {
+        return 0;
+    }
+}
