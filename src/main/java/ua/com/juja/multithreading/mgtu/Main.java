fb02aaf5d121ff2bf0d2918dde0b7f2158ebe086
Sergii Zagryvyi <serzhcello@gmail.com>
2016-12-14 15:20:48 +0200

    start with concurrent

--- src/main/java/ua/com/juja/multithreading/mgtu/Main.java
+++ src/main/java/ua/com/juja/multithreading/mgtu/Main.java
@@ -1,16 +1,17 @@
 package ua.com.juja.multithreading.mgtu;
 
 import ua.com.juja.multithreading.mgtu.interference.InterferenceExample;
+import ua.com.juja.multithreading.mgtu.run.SeriesRunExample;
 
 /**
  * Created by Serzh on 12/4/16.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         long begin = System.currentTimeMillis();
 //        RundomRunExample.example();
-
-        new InterferenceExample().example();
+        SeriesRunExample.example();
+//        new InterferenceExample().example();
 
         long end = System.currentTimeMillis();
         System.out.println("Time: " + (end - begin));
