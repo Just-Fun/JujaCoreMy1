9eb9ae738ed5741efa0e6cb5d90c8ac9c42b224a
Sergii Zagryvyi <serzhcello@gmail.com>
2017-01-09 18:13:58 +0200

    temp

---
+++ src/main/java/ua/com/juja/multithreading/elizarov_roman/SimpleTest1.java
@@ -0,0 +1,12 @@
+package ua.com.juja.multithreading.elizarov_roman;
+
+
+//import org.openjdk.jcstress.annotations.*;
+/**
+ * Created by Serzh on 1/9/17.
+ */
+
+//@JCStressTest
+//@State
+public class SimpleTest1 {
+}
